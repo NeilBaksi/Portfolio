f7f30162d45e2531277f4cab30f3fc1e14d78817
Neil Baksi <neil@oaklas.com>
2019-09-25 14:07:09 +1000

    updated portfolio

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,30 +2,26 @@ import React, { Component } from 'react';
 import Jobs from './Jobs';
 
 class Card extends Component {
-  render() {
-    const { work } = this.props.resumeObj;
-    const bgColors = ['#4A4A4A', '#DB7093', '#009EE2', '#FFAB91'];
-   
-    return (
-      <div className="Card-Container">
-      <h2 className="down-here animated bounce infinite">⬇ Down Here ⬇</h2>
-        <div className="Card-content">
-         {
-            work
-              .map((key, index) => {
-                return (
-                  <Jobs 
-                  key={index} 
-                  details={work[index]}
-                  colors={bgColors[index]}
-                  />
-                )
-              })
-          }
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const { work } = this.props.resumeObj;
+        const bgColors = ['#4A4A4A', '#DB7093', '#009EE2', '#FFAB91'];
+
+        return (
+            <div className='Card-Container'>
+                <div className='Card-content'>
+                    {work.map((key, index) => {
+                        return (
+                            <Jobs
+                                key={index}
+                                details={work[index]}
+                                colors={bgColors[index]}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Card;
