d4f01a8fd34a3c0695811d3f253c9104c53a25e6
Neil Baksi <neil@oaklas.com>
2022-06-27 17:20:19 +0530

    >

---
+++ build/precache-manifest.0f5c3bbd5afffcdd551124c589a5e48c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333164d4a8fbd982e74e2e10811e7b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13d6a69d38a6bb7c2e6a",
+    "url": "/static/css/main.9014be95.chunk.css"
+  },
+  {
+    "revision": "b90ae55fe72ac1532410",
+    "url": "/static/js/2.0de0d0d9.chunk.js"
+  },
+  {
+    "revision": "ea0e18386074927177213ab3ce19aa96",
+    "url": "/static/js/2.0de0d0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d6a69d38a6bb7c2e6a",
+    "url": "/static/js/main.b0250dea.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b0250dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad712fb5a9346b392d6",
+    "url": "/static/js/runtime-main.5c51c461.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70b061e206da90818237af871dfba4c1.js"
+  "/precache-manifest.0f5c3bbd5afffcdd551124c589a5e48c.js"
 );
 
 self.addEventListener('message', (event) => {
